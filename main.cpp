42e5346fcb10f6f14c90a30ac239f23f5b1f30d7
NotLuka <papez.luka@gmail.com>
2021-04-28 19:10:47 +0200

    narejena gibalna količina

--- main.cpp
+++ main.cpp
@@ -21,17 +21,17 @@ int main()
     sf::RenderWindow window(sf::VideoMode(x, y), "Particle collison", sf::Style::Close);
 
     vector<Particle> particles;
-    for(int i = 0; i < 60; i++)particles.push_back(Particle(x, y));
+    for(int i = 0; i < 200; i++)particles.push_back(Particle(x, y, particles));
 
 
-    // particles[0].pos = {850, 300};
+    // particles[0].pos = {300, 300};
     // particles[0].radius = 10;
-    // particles[0].v = {0.5, 0};
+    // particles[0].v_ = {0.5, 0};
 
 
     // particles[1].pos = {900, 300};
-    // particles[1].radius = 15;
-    // particles[1].v = {-0.5, 0};
+    // particles[1].radius = 20;
+    // particles[1].v_ = {-0.5, 0};
 
 
     while(window.isOpen())
@@ -40,19 +40,27 @@ int main()
 
         size = particles.size();
         auto start = chrono::system_clock::now();
-        for(int i = 0; i < size; i++)
-        {   //cout << particles[i].v[0] << " " << particles[i].v[1] << endl;
-            for(int j = 0; j < 100; j++)
+        for(int j = 0; j < 100; j++)
+        {   
+            for(int i = 0; i < size; i++)
             {   
                 particles[i].collision(particles);
-                particles[i].update_position(x, y, 300);
             }
-            sf::CircleShape shape = particles[i].draw();
-            window.draw(shape);
+            for(int i = 0; i < size; i++)
+            {   
+                particles[i].update_position(x, y, 100);
+            }
         }
         auto end = chrono::system_clock::now();
         cout << chrono::duration_cast<chrono::milliseconds>(end - start).count() << endl;
-        
+
+        for(int i = 0; i < size; i++)
+        {
+            sf::CircleShape shape = particles[i].draw();
+            window.draw(shape);
+        }
+
+
         sf::Event event;
         while (window.pollEvent(event))
         {
--- particle.cpp
+++ particle.cpp
@@ -7,34 +7,49 @@ class Particle {
     public:
         Vector pos;
         Vector v;
-        double radius; Vector center;
+        Vector v_;
+        double radius = 0; Vector center;
         unordered_set<int> collisions;
 
-    Particle(int x_size, int y_size)
-        {
+    Particle(int x_size, int y_size, vector<Particle> all)
+    {
         bool overlap = true;
+        while(overlap)
+        {
+            pos[0] = rand() % x_size;
+            pos[1] = rand() % y_size;
 
-        pos[0] = rand() % x_size;
-        pos[1] = rand() % y_size;
-
-        //radius = rand() % 5 + 5;
-        radius = 10;
-        center = pos + (Vector (cos(45), sin(45)) * radius);
+            while(radius == 0){radius = rand() % 20 + 5;}
+            center = pos + (Vector (cos(45), sin(45)) * radius);
+            overlap = check_overlap(all);
+        }
 
         v[0] = rand() % 10 - 5;
         v[1] = rand() % 10 - 5;
+        v_ = v;
+        
+    }
+
+    bool check_overlap(vector<Particle> all)
+    {
+        for(Particle particle : all)
+        {
+            if((pow(particle.center[0] - center[0], 2) + pow(particle.center[1] - center[1], 2)) <= particle.radius + radius)return true;
         }
+        return false;
+    }
 
     void update_position(int x_size, int y_size, double delta)
     {  
+        v = v_;
         pos = pos + v / delta;
         center = pos + (Vector (cos(45), sin(45)) * radius); 
 
-        if (center[0] - radius < 0) {pos[0] += - (center[0] - radius) / 2; v[0] = -v[0];}
-        if (center[1] - radius < 0) {pos[1] += - (center[1] - radius) / 2; v[1] = -v[1];}
+        if (center[0] - radius < 0) {pos[0] += - (center[0] - radius) / 2; v_[0] = -v[0];}
+        if (center[1] - radius < 0) {pos[1] += - (center[1] - radius) / 2; v_[1] = -v[1];}
 
-        if (x_size < center[0] + radius){pos[0] -= 2 * (center[0] + radius - x_size); v[0] = -v[0];}
-        if (y_size < center[1] + radius){pos[1] -= 2 * (center[1] + radius - y_size); v[1] = -v[1];}
+        if (x_size < center[0] + radius){pos[0] -= 2 * (center[0] + radius - x_size); v_[0] = -v[0];}
+        if (y_size < center[1] + radius){pos[1] -= 2 * (center[1] + radius - y_size); v_[1] = -v[1];}
 
     }
 
@@ -49,46 +64,31 @@ class Particle {
             {   
                 collisions.insert(i);
                 if(!in)
-                {
-                    // double m1 = radius;
-                    // double m2 = particle.radius;
+                {   
+                    double d;
+                    double m1 = radius;
+                    double m2 = particle.radius;
 
-                    // double k1 = (m1 - m2) / (m1 + m2);
-                    // double k2 = (2 * m2) / (m1 + m2);
+                    double k1 = (m1 - m2) / (m1 + m2);
+                    double k2 = (2 * m2) / (m1 + m2);
 
-                    // v = v * k1 + particle.v * k2;
 
-                    double x_collision = particle.pos[0] - (abs(particle.pos[0] - pos[0]) / (radius + particle.radius)) * particle.radius;
-                    double y_collision = particle.pos[1] - (abs(particle.pos[1] - pos[1]) / (radius + particle.radius)) * particle.radius;
-                    
-                    double k;
-                    double dx;
-                    double dy;
-                    if((y_collision - pos[1]) == 0)
-                    {
-                        dy = 10;
-                        dx = 0;
+                    Vector b = center * (-1) + particle.center;b = b / b.length();
 
-                    }else 
-                        {
-                    
-                            k = (pos[0] - x_collision) / (y_collision - pos[1]);
+                    Vector a = Vector(b[1], -b[0]);
 
-                            double n = y_collision - k * x_collision;
 
-                            double new_x = -100;
-                            double new_y = k * (new_x) + n;
+                    Vector vn = b * (v * b); Vector vn1 = b * (particle.v * b);
 
-                            dy = y_collision - new_y;
-                            dx = x_collision - new_x;
-                        }
+                    Vector vt = a * (v * a);//Vector vn1 = a * (particle.v * a);
 
-                    Vector b = Vector(dy, -dx);
-                    b = b / b.length();
-                    v = v + b * (v * b * -2);
+                    vn = vn * k1 + vn1 * k2;
+                    v_ = vn + vt;
+
+
                 }
 
-            }else{if(in)collisions.erase(i);}
+            }else{if(in){collisions.erase(i);}}
         }
     }
 
